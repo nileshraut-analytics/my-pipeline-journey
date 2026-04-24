f76ad0a0858e9393bdcf7189603fa98a77f418e6
Nilesh Raut <nr9837191@gmail.com>
2026-04-24 07:12:06 +0530

    Add data cleaning practice day9

--- day9.py
+++ day9.py
@@ -3,5 +3,10 @@ import pandas as pd
 df = pd.read_csv("sales_input.csv")
 df["amount"] = pd.to_numeric(df["amount"], errors="coerce")
 
-# Real world: find apple sales with amount > 50
-print(df.query("product == 'apple' and amount > 50"))
+bad_rows = df[df.isnull().any(axis = 1)]
+clean_rows = df.dropna()
+
+print("bad rows: ")
+print(bad_rows)
+print("\nclean rows")
+print(clean_rows)
