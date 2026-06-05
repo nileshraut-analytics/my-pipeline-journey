ee16addb709232c6f0ddca2a1efcec7542eae888
Nilesh Raut <nr9837191@gmail.com>
2026-06-05 08:31:16 +0530

    Replace CSV ingestion with PostgreSQL source

--- postgres_to_pandas.py
+++ postgres_to_pandas.py
@@ -13,7 +13,45 @@ conn = psycopg2.connect(
 # rows = cursor.fetchall()
 # df = pd.DataFrame(rows, columns=["product", "amount"])
 
-query = "SELECT * FROM sales"
-df = pd.read_sql(query, conn)
-print(df)
+def load_data():
+    query = "SELECT * FROM sales"
+    df = pd.read_sql(query, conn)
+    print(f"Loaded {len(df)} rows from postgreSQL")
+    return df
 
+def validate_data(df):
+    df["amount"] = pd.to_numeric(df["amount"],errors="coerce")
+    df["product"] = df["product"].str.strip().str.lower()
+    bad_rows = df[df.isnull().any(axis = 1)].copy()
+    clean_rows = df.dropna().copy()
+    print(f"loaded {len(bad_rows)} rows as Bad Data from Raw Data Source")
+    print(f"loaded {len(clean_rows)} rows as Clean Data from Raw Data Source")
+    return bad_rows, clean_rows
+
+def aggregate_data(clean_rows):
+    final_df = clean_rows.groupby("product")["amount"].agg(
+        count = "count",
+        total = "sum",
+        max = "max",
+        min = "min"
+    ).reset_index()
+    print(f"aggregated {len(final_df)} products from {len(clean_rows)} Clean Rows")
+    return final_df
+
+def clean_output(final_df):
+    print(f"Writing clean output CSV")
+    return final_df.to_csv("new_output.csv", index=False)
+
+def errors_log(bad_rows):
+    print(f"Writing errors log CSV")
+    return bad_rows.to_csv("errors_log.csv", index=False)
+
+def run_pipeline():
+    df = load_data()
+    bad_rows, clean_rows = validate_data(df)
+    final_df = aggregate_data(clean_rows)
+    clean_output(final_df)
+    errors_log(bad_rows)
+    print("Pipeline Completed Successfully!")
+
+run_pipeline()
