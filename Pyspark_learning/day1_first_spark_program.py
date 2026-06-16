90cdb343a0b46f974ab5a611ed1b34ad585215bd
Nilesh Raut <nr9837191@gmail.com>
2026-06-16 15:35:35 +0530

    day1: pyspark setup fixed and first dataframe working

--- Pyspark_learning/day1_first_spark_program.py
+++ Pyspark_learning/day1_first_spark_program.py
@@ -1,4 +1,26 @@
+
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("First Spark Program").getOrCreate()
-print(spark)
+# Create a Spark Session
+spark = SparkSession.builder \
+    .appName("My Pipeline Journey") \
+    .getOrCreate()
+
+# Sample data
+data = [
+    ("Alice", 25),
+    ("Bob", 30),
+    ("Charlie", 28)
+]
+
+# Column names
+columns = ["name", "age"]
+
+# Create Spark DataFrame
+df = spark.createDataFrame(data, columns)
+
+# Display the DataFrame
+df.show()
+
+# Stop the Spark Session
+spark.stop()
