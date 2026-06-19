dbb7c24057b2ceb723a7fe40dacaba0a98765bf9
Nilesh Raut <nr9837191@gmail.com>
2026-06-19 08:22:45 +0530

    Day 3: groupBy + agg with multiple aggregations using F.count/sum/max/min/avg

--- Pyspark_learning/day3_groupBy_&_agg.py
+++ Pyspark_learning/day3_groupBy_&_agg.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-
+from pyspark.sql import functions as F
 
 spark = SparkSession.builder.appName("My Pipeline Journey").getOrCreate()
 
@@ -14,13 +14,15 @@ data = [
 columns = ["Name", "Age", "Department", "Salary"]
 
 df = spark.createDataFrame(data, columns)
-df.show()
-df.printSchema()
-print(df.columns)
-print(df.dtypes)
-print(df.count())
-df.select("name").show()
-df.filter(df.Age>26).show()
+
+# df.groupBy("Department").agg({"Salary" : "avg"}).show()
+df.groupBy("Department").agg(
+    F.count("Salary").alias("count_salary"),
+    F.sum("Salary").alias("total_salary"),
+    F.max("Salary").alias("max_salary"),
+    F.min("Salary").alias("min_salary"),
+    F.avg("Salary").alias("average_salary")
+).show()
 
 spark.stop()
 
