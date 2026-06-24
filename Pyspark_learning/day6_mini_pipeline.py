c15c4716d8c6e856a149023408c502e6bc3f7cf8
Nilesh Raut <nr9837191@gmail.com>
2026-06-24 07:51:50 +0530

    Enhance README and add project dependencies

---
+++ Pyspark_learning/day6_mini_pipeline.py
@@ -0,0 +1,20 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+spark = SparkSession.builder.appName("File Handling").getOrCreate()
+
+df = spark.read.csv("sales_input.csv", header=True)
+
+def validate_data(df):
+    df = df.filter()
+
+final_df = df.groupBy("product").agg(
+    F.count("product").alias("product_count"),
+    F.sum("price").alias("total_price"),
+    F.max("price").alias("max_amount"),
+    F.min("price").alias("min_amount"),
+    F.avg("price").alias("average_amount")
+)
+
+final_df.toPandas().to_csv("output.csv", index=False)
+
